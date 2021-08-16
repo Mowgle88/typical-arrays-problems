8339b4aa53c1375d8a434a8584c0ab9dbe193c96
Mowgle88 <mowgle1988@gmail.com>
2021-08-16 16:55:02 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {return 0};
+  array.sort((prev, next) => prev - next);
+  return array[0]
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {return 0};
+  array.sort((prev, next) => next - prev);
+  return array[0]
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {return 0};
+  let sum = array.reduce((a, b) => a + b, 0);
+	return sum/array.length;
 }
